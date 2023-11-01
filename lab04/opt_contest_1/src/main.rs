7afcefdf6c4e14b0f2b1aafcee1d4c5a5026d8ed
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2023-11-01 23:07:25 +0200

    +opt_contest_1

--- lab04/opt_contest_1/src/main.rs
+++ lab04/opt_contest_1/src/main.rs
@@ -12,7 +12,7 @@ fn my_rot13() -> Result<(),io::Error> {
         } else if (i >= 'n' && i <= 'z') || (i >= 'N' && i <= 'Z') {
             code = i as u8 - 13;
         } else if i >= ' ' && i <= '~' {
-            continue;
+            code = i as u8;
         }
         else {
             return Err(my_err);
