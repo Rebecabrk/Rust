6ba4c7d3b03a594fda2f4e0de112e386beba456a
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2023-11-01 23:59:06 +0200

    inca nu e gata ex asta

--- lab04/p4/src/main.rs
+++ lab04/p4/src/main.rs
@@ -3,29 +3,47 @@ use std::{io, fs};
 
 fn main() -> Result<(), io::Error> {
     let s = fs::read_to_string("C:\\Windows\\System32\\drivers\\etc\\protocol")?;
+    let mut new_s = String::new();
     let mut index : usize ;
-    let mut column1 = String::new();
-    let mut column2 = String::new();
+    //let mut name = String::new();
+    //let mut number = String::new();
+    let mut ok = 1;
+    let mut third_column_index;
     for lines in s.lines(){
+        if ok == 1{
+            if lines.find("IP").is_some() {
+                third_column_index = lines.find("IP").unwrap();
+            }
+            else{
+                continue;
+            }
+        }
+        else {
+            continue;
+        }
+
+        ok += 1;
+        
         if lines.find('#').is_some() {
             index = lines.find('#').unwrap();
         }
         else  {
             //nu exista #
-            println!("{lines}");
+            //println!("{lines}");
             index = 0;
+            new_s.push_str(&lines[0..third_column_index]);
         }
         
         if lines.is_empty(){
+            //ignoram
             continue;
         } 
         else if index >= 1{
             //exista #, dar nu e la inceput
-            //println!("{lines}");
-            for coloumn in lines.splitn(3,' '){
-                //column1.push_str(coloumn)
-            }
+            new_s.push_str(&lines[0..third_column_index]);
         }
     }
+    
+    println!("{new_s}");
     Ok(())
 }
