669826bb7fa21fba5cc344f6a80b8c714b0e633e
Rebecabrk <125959502+Rebecabrk@users.noreply.github.com>
2023-11-02 20:35:01 +0200

    lab04

--- lab04/p4/src/main.rs
+++ lab04/p4/src/main.rs
@@ -1,49 +1,28 @@
 use std::{io, fs};
 
-
 fn main() -> Result<(), io::Error> {
     let s = fs::read_to_string("C:\\Windows\\System32\\drivers\\etc\\protocol")?;
-    let mut new_s = String::new();
     let mut index : usize ;
-    //let mut name = String::new();
-    //let mut number = String::new();
-    let mut ok = 1;
-    let mut third_column_index;
-    for lines in s.lines(){
-        if ok == 1{
-            if lines.find("IP").is_some() {
-                third_column_index = lines.find("IP").unwrap();
-            }
-            else{
-                continue;
-            }
+    let mut ignore;
+    for line in s.lines(){
+        if line.starts_with('#'){
+            ignore = true;
         }
         else {
-            continue;
+            ignore = false;
         }
-
-        ok += 1;
-        
-        if lines.find('#').is_some() {
-            index = lines.find('#').unwrap();
-        }
-        else  {
-            //nu exista #
-            //println!("{lines}");
-            index = 0;
-            new_s.push_str(&lines[0..third_column_index]);
-        }
-        
-        if lines.is_empty(){
-            //ignoram
-            continue;
-        } 
-        else if index >= 1{
-            //exista #, dar nu e la inceput
-            new_s.push_str(&lines[0..third_column_index]);
+        index = 0;
+        for word in line.split_ascii_whitespace(){
+            if ignore == false{
+                index += 1;
+                if index == 1 {
+                    print!("{word} => ");
+                }
+                else if index == 2 {
+                    println!("{word}");
+                }
+            }
         }
     }
-    
-    println!("{new_s}");
     Ok(())
 }
